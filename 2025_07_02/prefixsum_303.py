768e729fbfd18ec7e0c820964289189ea95822a0
Xiang Guo <xiangguoxg@gmail.com>
2025-07-02 16:27:22 -0400

    [2025-07-02] Prefix Sum

---
+++ 2025_07_02/prefixsum_303.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/range-sum-query-immutable/description/
+"""
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of `nums` between indices `left` and `right` inclusive where `left <= right`.
+Implement the `NumArray` class:
+
+* `NumArray(int[] nums)` Initializes the object with the integer array `nums`.
+* `int sumRange(int left, int right)` Returns the sum of the elements of `nums` between indices `left` and `right` inclusive
+  (i.e. `nums[left] + nums[left + 1] + ... + nums[right]`).
+"""
+from typing import List
+
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        return
+
+    def sumRange(self, left: int, right: int) -> int:
+        return -1
