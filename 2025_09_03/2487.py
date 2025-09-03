ba0afb73799c79bcb1665d6412523453419a214c
Xiang Guo <xiangguoxg@gmail.com>
2025-09-03 19:17:52 -0400

    add 2487

---
+++ 2025_09_03/2487.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/remove-nodes-from-linked-list/description/
+"""
+You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+"""
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return None
