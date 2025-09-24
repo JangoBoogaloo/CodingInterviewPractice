5f5ad85c4885558b70f2f5f4e9625d082b41a4ef
Xiang Guo <xiangguoxg@gmail.com>
2025-09-24 17:32:15 -0400

    Add 3205

---
+++ 2025_09_24/3205.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/maximum-array-hopping-score-i/description/
+"""
+Given an array nums, you have to get the maximum score starting from index 0 and hopping until you reach the last element of the array.
+In each hop, you can jump from index i to an index j > i, and you get a score of (j - i) * nums[j].
+Return the maximum score you can get.
+Example 1:
+Input: nums = [1,5,8]
+Output: 16
+Explanation:
+There are two possible ways to reach the last element:
+0 -> 1 -> 2 with a score of (1 - 0) * 5 + (2 - 1) * 8 = 13.
+0 -> 2 with a score of (2 - 0) * 8 = 16.
+Constraints:
+2 <= nums.length <= 10^3
+1 <= nums[i] <= 10^5
+"""
+from typing import List
+
+
+class SolutionTopDown:
+    def maxScore(self, nums: List[int]) -> int:
+        return -1
+
+
+class SolutionBottomUp:
+    def maxScore(self, nums: List[int]) -> int:
+        return -1
+
+
+class SolutionMemoryOptimize:
+    def maxScore(self, nums: List[int]) -> int:
+        return -1
