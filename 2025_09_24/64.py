4f35e9c2b6651bc0cc7686da3954c6b69823f0d0
Xiang Guo <xiangguoxg@gmail.com>
2025-09-24 17:50:38 -0400

    solution

--- 2025_09_24/64.py
+++ 2025_09_24/64.py
@@ -25,7 +25,22 @@ from typing import List
 
 class SolutionTopDown:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        return -1
+        minSumAt = [[-1] * len(grid[0]) for _ in range(len(grid))]
+        minSumAt[0][0] = grid[0][0]
+
+        def getSumAt(row, col) -> int:
+            if not row and not col:
+                return minSumAt[0][0]
+            if minSumAt[row][col] != -1:
+                return minSumAt[row][col]
+            if not row:
+                return grid[row][col] + getSumAt(row, col-1)
+            if not col:
+                return grid[row][col] + getSumAt(row-1, col)
+            minSumAt[row][col] = grid[row][col] + min(getSumAt(row, col-1), getSumAt(row-1, col))
+            return minSumAt[row][col]
+
+        return getSumAt(len(grid) -1, len(grid[0]) - 1)
 
 
 class SolutionBottomUp:
@@ -35,4 +50,12 @@ class SolutionBottomUp:
 
 class SolutionMemoryOptimize:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        return -1
+        for row in range(1, len(grid)):
+            grid[row][0] += grid[row-1][0]
+        for col in range(1, len(grid[0])):
+            grid[0][col] += grid[0][col-1]
+
+        for row in range(1, len(grid)):
+            for col in range(1, len(grid[0])):
+                grid[row][col] += min(grid[row-1][col], grid[row][col-1])
+        return grid[-1][-1]
