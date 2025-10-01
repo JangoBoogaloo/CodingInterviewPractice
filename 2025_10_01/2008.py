d2a1e2de601134e05dd95542967a362cc8390367
Xiang Guo <xiangguoxg@gmail.com>
2025-10-01 19:23:14 -0400

    explain

--- 2025_10_01/2008.py
+++ 2025_10_01/2008.py
@@ -14,6 +14,35 @@ You may only drive at most one passenger at a time.
 Given `n` and `rides`, return the maximum number of dollars you can earn by picking up the passengers optimally.
 
 Note: You may drop off a passenger and pick up a different passenger at the same point.
+
+
+Example 1:
+
+Input: n = 5, rides = [[2,5,4],[1,5,1]]
+Output: 7
+1    5        => 4
+ 2   5        => 1
+
+Explanation: We can pick up passenger 0 to earn 5 - 2 + 4 = 7 dollars.
+
+
+Example 2:
+
+Input: n = 20, rides = [[1,6,1],[3,10,2],[10,12,3],[11,12,2],[12,15,2],[13,18,1]]
+Output: 20
+Explanation: We will pick up the following passengers:
+- Drive passenger 1 from point 3 to point 10 for a profit of 10 - 3 + 2 = 9 dollars.
+- Drive passenger 2 from point 10 to point 12 for a profit of 12 - 10 + 3 = 5 dollars.
+- Drive passenger 5 from point 13 to point 18 for a profit of 18 - 13 + 1 = 6 dollars.
+We earn 9 + 5 + 6 = 20 dollars in total.
+
+1     6                                         1
+  3       10                                    2*     => 10 - 3 + 2
+          10    12                              3*     => 12 - 10 + 3
+             11 12                              2      => 12 - 11 + 2
+                12       15                     2      => 15 - 12 + 2
+                   13             18            1*     => 18 - 13 + 1
+
 """
 from typing import List
 
