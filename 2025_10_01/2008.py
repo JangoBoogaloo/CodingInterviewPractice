f0b6f10978d161910c189461bd82b809f3061fbd
Xiang Guo <xiangguoxg@gmail.com>
2025-10-01 20:25:37 -0400

    result

--- 2025_10_01/2008.py
+++ 2025_10_01/2008.py
@@ -20,8 +20,8 @@ Example 1:
 
 Input: n = 5, rides = [[2,5,4],[1,5,1]]
 Output: 7
-1    5        => 4
- 2   5        => 1
+1    5        => 1
+ 2   5        => 4
 
 Explanation: We can pick up passenger 0 to earn 5 - 2 + 4 = 7 dollars.
 
@@ -36,23 +36,79 @@ Explanation: We will pick up the following passengers:
 - Drive passenger 5 from point 13 to point 18 for a profit of 18 - 13 + 1 = 6 dollars.
 We earn 9 + 5 + 6 = 20 dollars in total.
 
-1     6                                         1
+cost per trip: 
+(end_i - start_i + tip_i)
+
+
+
+1      6                                         1
   3       10                                    2*     => 10 - 3 + 2
           10    12                              3*     => 12 - 10 + 3
              11 12                              2      => 12 - 11 + 2
                 12       15                     2      => 15 - 12 + 2
                    13             18            1*     => 18 - 13 + 1
 
+maxProfitTillHere[loc] = maxProfitTillHere[loc - 1]                   
+
+maxProfitTillHere[end_i] = max(maxProfitTillHere[end_i], maxProfitTillHere[start_i] + (end_i - start_i + tip_i))
 """
 from typing import List
 
 
 class SolutionDP:
     def maxTaxiEarnings(self, locations: int, rides: List[List[int]]) -> int:
-        return -1
+        
+        # sort by destinations
+        rides = sorted(rides, lambda x: x[1])
 
+        maxProfitTillHere = [0] * locations
 
+        # curr ride index
+        curr = 0
+
+        for i in range(locations):                        
+            maxProfitTillHere[i] = maxProfitTillHere[i - 1]                   
+
+            while curr < len(rides) and rides[curr][1] == i:
+                start_i, end_i, tip_i = rides[curr]
+                maxProfitTillHere[end_i] = max(maxProfitTillHere[end_i], maxProfitTillHere[start_i] + (end_i - start_i + tip_i))
+                curr+=1
+        return maxProfitTillHere[-1]
+
+import heapq
 
 class SolutionPQ:
     def maxTaxiEarnings(self, locations: int, rides: List[List[int]]) -> int:
-        return -1
+        rides.sort()
+        endHeap = []
+        for start, end, tip in rides:
+            currTripProfit = tip + end - start
+            while endHeap and endHeap[0][0] <= start:
+                maxProfit = max(maxProfit, endHeap[0][1])
+                heapq.heappop(endHeap)
+            maxProfit = currTripProfit + maxProfit
+            heapq.heappush(endHeap, (end, maxProfit))
+        
+        while endHeap:
+            maxProfit = max(maxProfit, endHeap[1])
+            endHeap.pop()
+
+        return maxProfit
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
