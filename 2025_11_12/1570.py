542723bd15c70f1f6aeaf45655e65cd2ed6d8624
Xiang Guo <xiangguoxg@gmail.com>
2025-11-09 22:22:21 -0500

    [2025-11-12] Data Structure Design

---
+++ 2025_11_12/1570.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/dot-product-of-two-sparse-vectors/description/
+"""
+Given two sparse vectors, compute their dot product.
+
+Implement class SparseVector:
+
+* SparseVector(nums) Initializes the object with the vector nums
+* dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+
+A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+Follow up: What if only one of the vectors is sparse?
+"""
+
+class SparseVector:
+    def __init__(self, nums):
+        return
+
+    def dotProduct(self, vec) -> int:
+        return -1
+
+
+
+
+
+
