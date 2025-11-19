27b88eeb6bd5b7bb475b85d9fad536ea6b2905c9
Xiang Guo <xiangguoxg@gmail.com>
2025-11-18 20:13:09 -0500

    [2025-12-03] Difficult to solve

---
+++ 2025_12_03/59/59.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/spiral-matrix-ii/description/
+"""
+59. Spiral Matrix II
+
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+"""
+from typing import List
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        return []
+
+
+
+
+
+
+
+
+
+
---
+++ 2025_12_03/959/959.py
@@ -0,0 +1,21 @@
+"""
+An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '. These characters divide the square into contiguous regions.
+
+Given the grid grid represented as a string array, return the number of regions.
+
+Note that backslash characters are escaped, so a '\' is represented as '\\'.
+
+Example1
+Input: grid = [" /","/ "]
+Output: 2
+
+Example2
+Input: grid = [" /","  "]
+Output: 1
+"""
+from typing import List
+
+
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        return -1
