3d052f90f887c3a7620d2e9b281171297ee9d19e
Xiang Guo <xiangguoxg@gmail.com>
2025-12-03 21:09:09 -0500

    result

--- 2025_12_03/59/59.py
+++ 2025_12_03/59/59.py
@@ -2,20 +2,98 @@
 """
 59. Spiral Matrix II
 
-Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n^2 in spiral order.
+
+n = 3
+123    (0,0) (0,1) (0,2)
+894                (1,2)
+765          (2,1) (2,2)
+
+12
+43
 """
 from typing import List
 
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        mat = [[0] * n] * n 
+
+        srows= 0, scols = 0
+        erows, ecols = n, n
+
+        def spiral(r, c, num, idir):
+
+            if mat[r][c] != 0:
+                idir += 1
+                idir %= 4 
+                return
+            
+            mat[r][c] = num + 1
+
+            if idir == 0:
+                spiral(r, c + 1, num)
+            if idir == 1:
+                spiral(r+1, c, num)
+            if idir == 2:
+                spiral(r, c-1, num)
+            if idir == 3:
+                spiral(r-1, c, num)
+            
         return []
 
 
 
 
+class Solution:
+    def _valid(self, newR, newC, matrix) -> bool:
+        if newR < 0 or newR >= len(matrix):
+            return False
+        if newC < 0 or newC >= len(matrix[0]):
+            return False
+        return matrix[newR][newC] == 0
+
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0] * n for _ in range(n)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        pos = (0, 0)
+        directionIndex = 0
+        matrix[0][0] = 1
+        currNum = 2
+
+        direction = directions[directionIndex]
+
+        while True:
+            newR, newC = pos[0] + direction[0], pos[1] + direction[1]
+            if self._valid(newR, newC, matrix):
+                matrix[newR][newC] = currNum
+                currNum += 1
+                pos = (newR, newC)
+            else:
+                directionIndex = (directionIndex+1) % len(directions)
+                direction = directions[directionIndex]
+                newR, newC = pos[0] + direction[0], pos[1] + direction[1]
+                if not self._valid(newR, newC, matrix):
+                    break
+        return matrix
 
 
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0] * n for _ in range(n)]
+        row, col = 0, 0
+        dr, dc = 0, 1
+        for num in range(n*n):
+            matrix[row][col] = num + 1
+            nr = (row + dr) % n
+            nc = (col + dc) % n
+            if matrix[nr][nc] != 0:
+                # [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                dr, dc = dc, -dr
+            row += dr
+            col += dc
+        return matrix
 
 
 
--- 2025_12_03/990/990.py
+++ 2025_12_03/990/990.py
@@ -6,6 +6,10 @@ Here, xi and yi are lowercase letters (not necessarily different) that represent
 Return true if it is possible to assign integers to variable names so as to satisfy all the given equations, or false otherwise.
 
 
+"a!=a"
+
+"a==b", "b==c", "a!=c"
+
 Example 1:
 
 Input: equations = ["a==b","b!=a"]
@@ -20,6 +24,9 @@ Input: equations = ["b==a","a==b"]
 Output: true
 Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
 
+# if check all ==, true  => set(a, b, c, d, e ...)
+
+# if check all !=,  x != y, true, x!= x false
 
 Constraints:
 
@@ -31,8 +38,73 @@ Constraints:
 * equations[i][3] is a lowercase letter.
 """
 from typing import List
+from collections import Counter, defaultdict
 
+"""
+a==b true
+a!=a false
+a==b, b!=a false
+a!=b, b!=c true
+
+"""
 
 class Solution:
+
     def equationsPossible(self, equations: List[str]) -> bool:
-        return False
+        
+        uf = UF()
+        
+        # filter out equals
+        equals = filter(equations, lambda x: x[1] == "=")
+        non_equals = filter(equations, lambda x: x[1] == "!")
+
+        for x, _, _, y in equals:
+            uf.add(x)
+            uf.add(y)
+            uf.union(x, y)
+        
+        for x, _, _, y in non_equals:
+            if not uf.valid(x) or not uf.valid(y):
+                continue
+            if x == y or uf.find(x) == uf.find(y):
+                return False
+        return True
+    
+
+class UF:
+    def __init__(self):
+        self._rank = Counter()
+        self._parent = defaultdict()
+        self._count = 0
+        return
+
+    def add(self, data) -> None:
+        if self._parent.get(data) is None:
+            self._parent[data] = data
+            self._count += 1
+
+    def valid(self, data) -> bool:
+        return self._parent.get(data) is not None
+
+    def find(self, data):
+        if self._parent.get(data) is None:
+            raise ValueError(f"data {data} is not added")
+        if self._parent.get(data) != data:
+            self._parent[data] = self.find(self._parent.get(data))
+        return self._parent[data]
+
+    def union(self, data1, data2) -> None:
+        data1Root, data2Root = self.find(data1), self.find(data2)
+        if data1Root == data2Root:
+            return
+        if self._rank[data1Root] > self._rank[data2Root]:
+            self._parent[data2Root] = data1Root
+        elif self._rank[data1Root] < self._rank[data2Root]:
+            self._parent[data1Root] = data2Root
+        else:
+            self._parent[data2Root] = data1Root
+            self._rank[data1Root] += 1
+        self._count -= 1
+
+    def __len__(self):
+        return self._count
