f48cfb7139a7566e0215166dafcc5278afc705ea
Xiang Guo <xiangguoxg@gmail.com>
2026-01-23 21:10:53 -0500

    solution

--- 2026_01_23/11.py
+++ 2026_01_23/11.py
@@ -1,7 +1,8 @@
 """
 11. Container With Most Water
 
-You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+You are given an integer array height of length n. 
+There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 
@@ -13,7 +14,28 @@ Example 1:
 
 Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
-Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Explanation: 
+* The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+* In this case, the max area of water (blue section) the container can contain is 49.
+
+  | 
+|*|
+|*|*| 
+|*|*|*|
+-----
+[3,0,10,0,2,0,1]
+
+[3,3] => 3
+[3,0,3] => 6
+
+
+[1,8,6,2,5,4,8,3,7]
+
+1,8 -> 1*1
+1,8,6 -> 6, 1*2
+1,8,6,2 -> 2, 2*2, 1*3
+
+1, min(any 2 wall heights) * distance btw two walls
 
 """
 from typing import List
@@ -21,4 +43,36 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        return -1
+
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            
+            dist = r - l
+            #[1,8,6,3,7]    7*3
+            # 1, 7   dist = 4 area = 4
+            #    8,7 dist = 3 area = 21
+            #    8,3 dist = 2 area = 6
+            #    
+            l_height, r_height = height[l], height[r]
+
+            if l_height > r_height:
+                res = max(res, dist * r_height)
+                r -= 1
+            elif l_height <= r_height:
+                res = max(res, dist * l_height)
+                l += 1
+            
+        return res
+    
+
+
+
+
+
+
+
+
+
+
--- 2026_01_23/1673.py
+++ 2026_01_23/1673.py
@@ -7,11 +7,52 @@ An array's subsequence is a resulting sequence obtained by erasing some (possibl
 
 We define that a subsequence a is more competitive than a subsequence b (of the same length) if in the first position where a and b differ,
 subsequence a has a number less than the corresponding number in b.
+
 For example, [1,3,4] is more competitive than [1,3,5] because the first position they differ is at the final number, and 4 is less than 5.
+
+Example 1:
+
+Input: nums = [3,5,2,6], k = 2
+Output: [2,6]
+Explanation: Among the set of every possible subsequence: {[3,5], [3,2], [3,6], [5,2], [5,6], [2,6]}, [2,6] is the most competitive.
+Example 2:
+
+Input: nums = [2,4,3,3,5,4,9,6], k = 4
+Output: [2,3,3,4]
+
+0, n-k
+
+1. most significant, lead to from left to right
+2. next num smaller, and have more thank k left 
+
+[1,9,2,1,1,0] k=3 => 110
+
+[9,1,2,1] k=3
+
+[1,2,1,2,0] k=4
+
+[1,2,3,0,9]
+
+
 """
 from typing import List
 
 
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        return []
+
+        incStk = []
+        n = len(nums)
+        allowed_ops = n - k
+
+        # [1,2,9,9,9,9,9,9,9,9,9,0]
+        for i in range(n):
+            curr = nums[i]
+
+            while incStk and incStk[-1] < curr and allowed_ops >= 0:
+                incStk.pop()
+                allowed_ops -= 1
+    
+            incStk.append(curr)
+
+        return incStk[:k]
